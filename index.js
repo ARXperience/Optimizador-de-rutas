acbd8d0be2b1a75c6484d76eacb52d00cb54a0a6
ARXperience <centrodigitaldediseno@gmail.com>
2025-01-16 06:08:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css"; // Si tienes un archivo de estilos globales
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// Crear un contenedor de la aplicación
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Renderizar el componente App en el contenedor
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
